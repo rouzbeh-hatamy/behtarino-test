80fffe8acace8c72d4364e0e61f040604db42695
roozbeh <roozbeh.hatamy@gmail.com>
2022-10-25 16:41:42 +0330

    add home page cards

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,62 @@
-import React from "react";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { Container } from "@mui/system";
+import { useRouter } from "next/router";
 
-function index() {
-  return <div>index</div>;
+export default function Home({ items }) {
+  const { push } = useRouter();
+  const handleClick = (id) => {
+    push(`/${id}`);
+  };
+  return (
+    <Container>
+      <Grid container>
+        <Grid item display="flex" flexWrap="wrap" py={4}>
+          {items.map((item) => (
+            <Card sx={{ maxWidth: 345, margin: 2 }} key={item.id}>
+              <CardMedia
+                component="img"
+                alt={item.name}
+                height="240"
+                image={item.image}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {item.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {item.description}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  size="small"
+                  variant="outlined"
+                  onClick={() => handleClick(item.id)}
+                >
+                  Buy Now
+                </Button>
+              </CardActions>
+            </Card>
+          ))}
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
-
-export default index;
+export async function getStaticProps(context) {
+  const res = await fetch(`https://fakestoreapi.com/products?limit=6`);
+  const items = await res.json();
+  return {
+    props: {
+      items,
+    },
+  };
+}
