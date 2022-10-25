f157aa55d064982d0d12134a38ca2fb61a87dc03
roozbeh <roozbeh.hatamy@gmail.com>
2022-10-25 15:12:32 +0330

    initial commit / add configs

---
+++ config/theme.js
@@ -0,0 +1,26 @@
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+// Create a theme instance.
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#556cd6',
+        },
+        secondary: {
+            main: '#19857b',
+        },
+        error: {
+            main: red.A400,
+        },
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+              root: {
+                textTransform:'none'
+              },
+            },
+          },
+    },
+});
+export default theme;
---
+++ pages/[itemId]/index.js
@@ -0,0 +1,11 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+
+function Item() {
+    const {query} =useRouter()
+  return (
+    <div>{query.itemId}</div>
+  )
+}
+
+export default Item
